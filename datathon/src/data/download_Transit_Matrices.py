403c383eaf890c24136c93ce219dd263106ea6f9
James Keane <yahmskeano@gmail.com>
2022-07-19 18:04:59 -0500

    Separate transit matrices by CE tract

--- datathon/src/data/download_Transit_Matrices.py
+++ datathon/src/data/download_Transit_Matrices.py
@@ -10,23 +10,22 @@ transit_urls = {
     'walking':'https://dl2.boxcloud.com/d/1/b1!wTq-kPEs9JHClABvi70oQY4j7hXVG-hmLklStQj6OR5gGcdfHlSSHW9ZYxowe513PwtxiDclS1L8Ru3n7U_Z9inEyKZVZ2CErLz54l_HSdW7jeuRwE_Y20uIHxo85tpZa6Q9nXs4KMkFS-EY8CLF2uhJZW-7UJQKyHMvFRaMsUgp-rMI-bLm8AE82r2kXlh-WcBda0emkhtrXt7MJPsGjV9i80wBbkPL1Qr7Jzlju4GgHpdBpUTPwIYTFveCAt-ts0BVbUWDvNXmOSGH_37wTgRY8_7cccLKqZ_wEHnZI8kbAVHoMu85evLkG9E9SXaUYOgF7kk5if-VqIMb5f3-C3sy2zCPbcJri49X1J_-vBjPVQh_MfW7hCfhKmUfgfFQwBg9RJu3sCa_1tsIHDi8D2it3qZ4RM_FoTZ5YLV6VAxPKgakNaFY8IRnuQW5CqS2aKACpBkk9-_mn5_j2MAEQfFOhRv--x_RSJ_dSf5WTfV8Qv42n1xmg6_YxMoTo7YBpQa6SVjhdipd-uc9mXwKy-1vQcms3XKwZlhDJcv9vDVjEl0duudFm459IAEOIB3fBbXPX-D0FkWMIWoreJ-3zfrqUYAremPSt4-nFS_VfODr6elZE171WXEbgORh_0V7UvbMe2koXaV0wiQwyLQnBj81lTgK789EtgaJnI7GtiaPhJF6Cobn1dM53xNtbxl21DDJysnaFnI0WEhqi3IYjvBbA8vMUHPyVnSLrjZFkFrVlQBVVHZPUmezxPgnnTdp_NC3OKgtnSGMOOEAREkw-n57I1f8sqQ-4tG-nYrKmehcwffQLVEMkXD4Z9ljNo0tnZoyxUxI857tKgQYPYGDw5X-hYG7JVu8jJmGcoWLsQDZ_5TDVW5ImU_LBQOxx5VsaD4tCQFubt4RerepLyouCAQEGslS3TXdjYGL_J5ys8dR2sAiYIy2QSN-_lKgFOEkBLb8gTpz0VBzJ1y1MZy3ixpNwk9KsAzijJkvkKLzrmbk-mygI560v7251Gr8e-ezGQe3fbsQ6ipxiBoqZSkujn-TEhxPmZO1X65Ty92OREb_mYm4Hp0aS5CMc38aZJ-2ane1s-dRuHpTHVDdk5dxeJ9wfT_DQu79YCEXZJ0V2qTznkpYq8x88jmYdcK6uGLV5gjjaBE7X0YmGAwqsnkf-c-SzoaUCT09GlsSZHCkSeYCJVuJY8SFADbOaZtYYkvBB7vwaflfWHC0l5XLaHNcKTYmn1ikNpxTXp6872sYWUe5V98_v8kWnT46WMI3OouWnHCsnfElbkluR17cgqJbf4Q./download'
 }
 
-def filter_matrices(city, data, tracts):
+def filter_matrices(city, data, tracts,type):
     tracts['tract_id'] = pd.to_numeric(tracts['tract_id'])
-    data = data[data['origin'].isin(tracts['tract_id'])]
-    data = data[data['destination'].isin(tracts['tract_id'])]
-    return data
-    
+
+    for i in range(len(tracts)):
+        tract = tracts['tract_id'][i]
+        temp = data[data['destination']==tract]
+        temp.to_csv(f".../mobility/data/transit matrices/{city}/{type}/{tract}.csv", index=False)
+    return  data
 
 def download_matrices():
     chi_tracts = gpd.read_file(os.path.normpath(os.path.join(os.path.dirname(__file__),'../data/processed/park_tract_join_CHICAGO.geojson')))
     nyc_tracts = gpd.read_file("../data/processed/park_tract_join_NYC.geojson")
     for name, url in transit_urls.items():
         data = pd.read_parquet(url)
-        data = data.iloc[:,1:]
-        chi_od = filter_matrices("CHICAGO", data, chi_tracts)
-        nyc_od = filter_matrices("NYC", data, nyc_tracts)
-        chi_od.to_csv(f".../mobility/data/transit matrices/CHI_{name}.csv",index=False)
-        nyc_od.to_csv(f".../mobility/data/transit matrices/NYC_{name}.csv",index=False)
+        chi_od = filter_matrices("CHICAGO", data, chi_tracts,type)
+        nyc_od = filter_matrices("NYC", data, nyc_tracts, type)
 
 
 
